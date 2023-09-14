0bc4b026f941e3a218c9f9458de6c37f83fafe0d
Clarence Jordan <clarencejordan087@gmail.com>
2023-09-13 20:26:43 -0500

    first vue app push task

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <ButtonItem @btn-click="$emit('toggle-add-task')" :text="boolean ? 'Close' : 'Add task'" :color="boolean? 'red':'green'" />
+    </header>
+</template>
+
+<script>
+import ButtonItem from './Button';
+
+export default {
+    name: 'HeaderItem',
+    props: {
+        title: String, // Define title as a string prop
+        boolean: Boolean // Define boolean as a boolean prop
+    },
+    components: {
+        ButtonItem,
+    }
+}
+</script>
+
+
+<!-- only for this component -->
+<style scoped>
+    header{
+        display:flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <div v-for="task  in tasks" :key="task.id">
+        <TaskComponent @toggle-reminder="$emit('toggle-reminder',task.id)" @delete-task = "$emit('delete-task',task.id)" :task = "task"/>
+    </div>
+</template>
+
+
+
+<script>
+import TaskComponent from './Tasks';
+    export default{
+        name:'TaskItem',
+        props:{
+            tasks:Array
+        },
+        components:{
+            TaskComponent
+        },
+        emits:['delete-task','toggle-reminder']
+    }
+
+</script>
